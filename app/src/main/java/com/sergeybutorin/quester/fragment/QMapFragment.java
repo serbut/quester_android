c0464724c955956d036181c675949631968dfc58
Alexandr Orel <4aorel@mail.ru>
2017-12-10 12:14:34 +0300

    Hide quest details on map click (issue #21)

--- app/src/main/java/com/sergeybutorin/quester/fragment/QMapFragment.java
+++ app/src/main/java/com/sergeybutorin/quester/fragment/QMapFragment.java
@@ -204,21 +204,26 @@ public class QMapFragment extends Fragment implements OnMapReadyCallback,
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng point) {
-                if (state == QUESTS_STATE.ADD) {
-                    Marker marker = mMap.addMarker(
-                            new MarkerOptions()
-                                    .position(point)
-                                    .title("title")
-                                    .snippet("snippet")
-                                    .draggable(true)
-                                    .icon(BitmapDescriptorFactory
-                                            .defaultMarker(BitmapDescriptorFactory.HUE_ROSE))
-                    );
-                    questToAdd.addPoint(point);
-                    questToAdd.addMarkers(marker);
+                switch (state){
+                    case DISPLAY:
+                        showQuests();
+                        break;
+                    case ADD:
+                        Marker marker = mMap.addMarker(
+                                new MarkerOptions()
+                                        .position(point)
+                                        .title("title")
+                                        .snippet("snippet")
+                                        .draggable(true)
+                                        .icon(BitmapDescriptorFactory
+                                                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE))
+                        );
+                        questToAdd.addPoint(point);
+                        questToAdd.addMarkers(marker);
 
-                    LatLng position = marker.getPosition();
-                    Log.d(TAG, position.latitude + ", " + position.longitude);
+                        LatLng position = marker.getPosition();
+                        Log.d(TAG, position.latitude + ", " + position.longitude);
+                        break;
                 }
             }
         });
